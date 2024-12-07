74ad033d4e552586d207ffd5d2c698b1ac8d3f70
Dimas <dimaskurniawan40@gmail.com>
2024-12-08 00:29:18 +0700

    fix(model): remove field "description" from content model contentful

--- src/lib/contentful.ts
+++ src/lib/contentful.ts
@@ -7,7 +7,6 @@ export interface BlogPost {
     title: EntryFieldTypes.Text,
     content: EntryFieldTypes.RichText,
     date: EntryFieldTypes.Date,
-    description: EntryFieldTypes.Text,
     slug: EntryFieldTypes.Text,
   }
 }
